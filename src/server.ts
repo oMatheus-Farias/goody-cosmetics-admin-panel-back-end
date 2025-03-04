02eff4735723ab86b3a4f1bb7306c18a93581b13
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-04 18:21:41 -0300

    chore: add env validate file

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import { app } from './app';
+import { env } from './configs/env';
 
 async function main() {
   try {
-    const host = await app.listen({ port: 8080 });
+    const PORT = env.PORT;
+
+    const host = await app.listen({ port: PORT });
     console.log(`Server listening at ${host} 🚀`);
   } catch (error) {
     console.error(error);
   }
 }
-
 main();
