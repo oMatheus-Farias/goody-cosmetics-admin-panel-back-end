32f9729eebb5a49ac87043d33fb26454eb33cfa5
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-04 17:30:47 -0300

    feat: add first commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { app } from "./app";
+
+async function main() {
+  try {
+    const host = await app.listen({ port: 8080 });
+    console.log(`Server listening at ${host} 🚀`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
