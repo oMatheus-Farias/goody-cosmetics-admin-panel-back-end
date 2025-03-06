f8702e7d8ada22d90eac5bafa32d27c59b842e38
Matheus <matheusfariasdasneves@gmail.com>
2025-03-05 21:36:38 -0300

    feat: add CreateCategoriesUseCase

---
+++ src/use-cases/categories/create.ts
@@ -0,0 +1,18 @@
+import type { Prisma } from '@prisma/client';
+
+import type { CategoriesRepository } from '../../database/repositories/interfaces';
+import { AlreadyExistsError } from '../../errors/already-exists-error';
+
+export class CreateCategoriesUseCase {
+  constructor(private readonly categoriesRepo: CategoriesRepository) {}
+
+  async execute(data: Prisma.CategoryCreateInput) {
+    const category = await this.categoriesRepo.findByName(data.name);
+
+    if (category) {
+      throw new AlreadyExistsError('Category already exists');
+    }
+
+    await this.categoriesRepo.create(data);
+  }
+}
