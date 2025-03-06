163e905972747a63c4c51981ef8bf4eb43822da3
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-06 19:59:26 -0300

    feat: add categoriesSchema validation

---
+++ src/libs/zod-schemas/categories-schemas.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+
+export const categoriesSchema = z.object({
+  name: z
+    .string({
+      required_error: 'Name is required and must be a string',
+    })
+    .trim()
+    .min(1, {
+      message: 'Name must have at least 1 character',
+    })
+    .max(255, {
+      message: 'Name must have at most 255 characters',
+    }),
+  emoji: z
+    .string({
+      required_error: 'Emoji is required and must be a string of 1 character',
+    })
+    .trim()
+    .min(1, {
+      message: 'Emoji must have at least 1 character',
+    })
+    .max(1, {
+      message: 'Emoji must have at most 1 character',
+    }),
+});
