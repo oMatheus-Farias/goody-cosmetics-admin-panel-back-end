1d5183a00041059e7b9adc3baf1a0a926c78677d
Matheus <matheusfariasdasneves@gmail.com>
2025-03-07 17:09:38 -0300

    feat: add UsersRepository interface

---
+++ src/database/repositories/interfaces/users-repository.ts
@@ -0,0 +1,39 @@
+import type { Prisma, User } from '@prisma/client';
+
+type TFindAllWithParams = {
+  users: User[] | null;
+  meta: {
+    pageIndex: number;
+    limit: number;
+    countPerPage: number;
+    totalCount: number;
+  };
+};
+
+export interface UsersRepository {
+  findById(
+    userId: string,
+  ): Promise<Pick<User, 'id' | 'firstName' | 'lastName'> | null>;
+  findByEmail(
+    email: string,
+  ): Promise<Pick<User, 'id' | 'email' | 'firstName' | 'lastName'> | null>;
+  findAllWithParams(
+    page: number,
+    searchTerm?: string,
+  ): Promise<TFindAllWithParams>;
+  create(data: Prisma.UserCreateInput): Promise<void>;
+  update(userId: string, data: Prisma.UserUpdateInput): Promise<void>;
+  updatePassword(
+    userId: string,
+    oldPassword: string,
+    newPassword: string,
+  ): Promise<void>;
+  forgotPassword(
+    userId: string,
+    resetToken: string,
+    resetTokenExpiresAt: Date,
+  ): Promise<void>;
+  resetPassword(userId: string, newPassword: string): Promise<void>;
+  setNullResetToken(userId: string): Promise<void>;
+  delete(userId: string): Promise<void>;
+}
