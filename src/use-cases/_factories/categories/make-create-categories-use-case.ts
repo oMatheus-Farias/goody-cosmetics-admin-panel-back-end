2d790a9a2fa7e89fc29a9cdc27af4bac1827fb91
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-08 10:57:45 -0300

    feat: add return typing in make function

--- src/use-cases/_factories/categories/make-create-categories-use-case.ts
+++ src/use-cases/_factories/categories/make-create-categories-use-case.ts
@@ -1,7 +1,7 @@
 import { PrismaCategoriesRepository } from '../../../database/repositories/prisma';
 import { CreateCategoriesUseCase } from '../../categories';
 
-export function makeCreateCategoriesUseCase() {
+export function makeCreateCategoriesUseCase(): CreateCategoriesUseCase {
   const categoriesRepo = new PrismaCategoriesRepository();
   return new CreateCategoriesUseCase(categoriesRepo);
 }
