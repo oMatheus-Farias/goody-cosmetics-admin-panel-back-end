3f16b02cbe03a650643fe1d6c0bedf87fbb3b355
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-08 12:48:53 -0300

    feat: add users schemas validation

---
+++ src/libs/zod-schemas/users-schemas.ts
@@ -0,0 +1,58 @@
+import { z } from 'zod';
+
+export const usersSchema = z.object({
+  firstName: z
+    .string({
+      required_error: 'First name is required',
+    })
+    .trim()
+    .min(2, {
+      message: 'First name is too short',
+    })
+    .max(55, {
+      message: 'First name is too long',
+    }),
+  lastName: z
+    .string({
+      required_error: 'Last name is required',
+    })
+    .trim()
+    .min(2, {
+      message: 'Last name is too short',
+    })
+    .max(55, {
+      message: 'Last name is too long',
+    }),
+  email: z
+    .string({
+      required_error: 'Email is required',
+    })
+    .email({
+      message: 'Invalid email',
+    }),
+  role: z
+    .enum(['ROOT', 'ADMIN'], {
+      required_error: 'Role is required',
+    })
+    .optional(),
+});
+
+export const usersFirstPasswordSchema = z.object({
+  password: z
+    .string({
+      required_error: 'Password is required.',
+    })
+    .min(8, {
+      message: 'Password must have at least 8 characters.',
+    })
+    .max(255, {
+      message: 'Password must have at most 255 characters.',
+    })
+    .regex(
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?";:{}|[~<>=_+-]).{8,}$/,
+      {
+        message:
+          'The password must have at least one uppercase and one lowercase letter, at least one number and one special character.',
+      },
+    ),
+});
