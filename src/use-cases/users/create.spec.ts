41b9123b56f467282a1c74d3c08a67a2fc1ebed5
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-10 19:21:08 -0300

    test: ensure be able create user

---
+++ src/use-cases/users/create.spec.ts
@@ -0,0 +1,38 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import type { PasswordHasher } from '../../adapters/interfaces/password-hasher';
+import { PasswordHasherAdapter } from '../../adapters/password-hasher-adapter';
+import { InMemoryUsersRepository } from '../../database/repositories/in-memory';
+import type { UsersRepository } from '../../database/repositories/interfaces';
+import { CreateUsersUseCase } from './create';
+
+let userRepo: UsersRepository;
+let passwordHasher: PasswordHasher;
+let sut: CreateUsersUseCase;
+const userData = {
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'johndoe@email.com',
+  passwordHash: 'password',
+};
+
+describe('Create User', () => {
+  beforeEach(async () => {
+    userRepo = new InMemoryUsersRepository();
+    passwordHasher = new PasswordHasherAdapter();
+    sut = new CreateUsersUseCase(userRepo, passwordHasher);
+
+    await sut.execute({
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      email: userData.email,
+      passwordHash: userData.passwordHash,
+    });
+  });
+
+  it('should be able create user', async () => {
+    const user = await userRepo.findByEmail(userData.email);
+
+    expect(user).not.toBe(null);
+  });
+});
