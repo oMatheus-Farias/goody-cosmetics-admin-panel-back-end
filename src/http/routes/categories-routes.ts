bee18f600a2034caae4afed1b7088e4f4d28c73f
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-14 16:25:12 -0300

    feat: add find all categories with params endpoint

--- src/http/routes/categories-routes.ts
+++ src/http/routes/categories-routes.ts
@@ -1,8 +1,16 @@
 import type { FastifyInstance } from 'fastify';
 
-import { createCategoriesController } from '../controllers/categories/create';
+import {
+  createCategoriesController,
+  findAllCategoriesWithParamsController,
+} from '../controllers/categories';
 import { verifyJwt } from '../middlewares/verify-jwt';
 
 export async function categoriesRoutes(app: FastifyInstance) {
+  app.get(
+    '/params',
+    { onRequest: [verifyJwt] },
+    findAllCategoriesWithParamsController,
+  );
   app.post('/', { onRequest: [verifyJwt] }, createCategoriesController);
 }
