cc246c922d463d8b0d0ff3c722e27ae4a9e215ba
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-15 14:43:46 -0300

    test: ensure be able delete user

---
+++ src/use-cases/users/delete.spec.ts
@@ -0,0 +1,41 @@
+import type { $Enums } from '@prisma/client';
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import { PasswordHasherAdapter } from '../../adapters';
+import type { PasswordHasher } from '../../adapters/interfaces';
+import { InMemoryUsersRepository } from '../../database/repositories/in-memory';
+import type { UsersRepository } from '../../database/repositories/interfaces/users-repository';
+import { CreateUsersUseCase } from './create';
+import { DeleteUsersUseCase } from './delete';
+
+let usersRepo: UsersRepository;
+let passwordHasher: PasswordHasher;
+let createUsersUseCase: CreateUsersUseCase;
+let sut: DeleteUsersUseCase;
+const userData = {
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'johndoe@email.com',
+  password: 'password',
+  role: 'ADMIN' as $Enums.UserRole,
+};
+
+describe('Delete Users', () => {
+  beforeEach(async () => {
+    usersRepo = new InMemoryUsersRepository();
+    passwordHasher = new PasswordHasherAdapter();
+    createUsersUseCase = new CreateUsersUseCase(usersRepo, passwordHasher);
+    sut = new DeleteUsersUseCase(usersRepo);
+
+    await createUsersUseCase.execute(userData);
+  });
+
+  it('should be able delete user', async () => {
+    const user = await usersRepo.findByEmail(userData.email);
+
+    await sut.execute(user!.id);
+    const deletedUser = await usersRepo.findById(user!.id);
+
+    expect(deletedUser).toBeNull();
+  });
+});
