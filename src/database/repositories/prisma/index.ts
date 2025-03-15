7b9d6030c05ba9e172f5890b3146adce7339fa71
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-15 18:08:02 -0300

    feat: add PrismaProductsRepository

--- src/database/repositories/prisma/index.ts
+++ src/database/repositories/prisma/index.ts
@@ -1,3 +1,4 @@
 export * from './prisma-categories-repository';
+export * from './prisma-products-repository';
 export * from './prisma-refresh-tokens-repository';
 export * from './prisma-users-repository';
---
+++ src/database/repositories/prisma/prisma-products-repository.ts
@@ -0,0 +1,154 @@
+import type { Prisma, Product } from '@prisma/client';
+
+import { prisma } from '../../../app';
+import type {
+  ProductsRepository,
+  TFindAllWithParams,
+  TProduct,
+} from '../interfaces';
+
+export class PrismaProductsRepository implements ProductsRepository {
+  async findById(productId: string): Promise<Pick<Product, 'id'> | null> {
+    return await prisma.product.findUnique({
+      where: { id: productId },
+      select: { id: true },
+    });
+  }
+  async findByName(productName: string): Promise<Pick<Product, 'id'> | null> {
+    return await prisma.product.findUnique({
+      where: { name: productName },
+      select: { id: true },
+    });
+  }
+  async findAll(): Promise<TProduct[] | null> {
+    return await prisma.product.findMany({
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        currentPrice: true,
+        oldPrice: true,
+        stockQuantity: true,
+        categories: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        productImage: {
+          select: {
+            url: true,
+          },
+        },
+      },
+    });
+  }
+  async findAllByCategory(categoryId: string): Promise<TProduct[] | null> {
+    return await prisma.product.findMany({
+      where: {
+        categoryId,
+      },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        currentPrice: true,
+        oldPrice: true,
+        stockQuantity: true,
+        categories: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        productImage: {
+          select: {
+            url: true,
+          },
+        },
+      },
+    });
+  }
+  async findAllWithParams(
+    page: number,
+    searchTerm?: string,
+  ): Promise<TFindAllWithParams> {
+    const products = await prisma.product.findMany({
+      skip: page * 10,
+      take: 10,
+      orderBy: { name: 'asc' },
+      where: {
+        name: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        currentPrice: true,
+        oldPrice: true,
+        stockQuantity: true,
+        categories: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        productImage: {
+          select: {
+            url: true,
+          },
+        },
+      },
+    });
+
+    const count = await prisma.product.count({
+      skip: page * 10,
+      take: 10,
+      where: {
+        name: {
+          contains: searchTerm,
+        },
+      },
+    });
+
+    const totalCount = await prisma.product.count({
+      where: {
+        name: {
+          contains: searchTerm,
+        },
+      },
+    });
+
+    return {
+      products,
+      meta: {
+        pageIndex: page,
+        limit: 10,
+        countPerPage: count,
+        totalCount,
+      },
+    };
+  }
+  async create(data: Prisma.ProductCreateInput): Promise<void> {
+    await prisma.product.create({
+      data,
+    });
+  }
+  async update(
+    productId: string,
+    data: Prisma.ProductUpdateInput,
+  ): Promise<void> {
+    await prisma.product.update({
+      where: { id: productId },
+      data,
+    });
+  }
+  async delete(productId: string): Promise<void> {
+    await prisma.product.delete({
+      where: { id: productId },
+    });
+  }
+}
