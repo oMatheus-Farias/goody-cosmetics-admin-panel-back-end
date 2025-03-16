97e030dd9c6f7036444a92d774713962c8db4f3e
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-16 19:40:55 -0300

    feat: add ConflictError custom error

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,4 +1,5 @@
 export * from './already-exists-error';
+export * from './conflict-error';
 export * from './credentials-error';
 export * from './not-found-error';
 export * from './wrong-password-length-error';
