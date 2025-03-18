4267a3be8d7d30da3ace70b075e9c4d8d4432950
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-18 16:47:22 -0300

    refactor: add extract form data function in createProductsController and remove extensive codes

--- src/http/controllers/products/create.ts
+++ src/http/controllers/products/create.ts
@@ -8,6 +8,7 @@ import {
   ConflictError,
   NotFoundError,
 } from '../../../errors';
+import { extractCreateProductsFormData } from '../../../functions/extract-create-products-form-data';
 import { createProductsSchema } from '../../../libs/zod-schemas/products-schemas';
 import { makeCreateProductsUseCase } from '../../../use-cases/_factories/products/make-create-products-use-case';
 
@@ -17,50 +18,35 @@ export async function createProductsController(
 ): Promise<FastifyReply | void> {
   try {
     const formData = (await request.formData()) as FormData;
-
-    //TODO: Refactor code
-    const name = formData.get('name') as string;
-    const description = formData.get('description') as string;
-    const categoryId = formData.get('categoryId') as string;
-    const oldPrice = Number(formData.get('oldPrice'));
-    const currentPrice = Number(formData.get('currentPrice'));
-    const stockQuantity = Number(formData.get('stockQuantity'));
-    const image01 = formData.get('image01') as File;
-    const image02 = formData.get('image02') as File;
+    const formFields = extractCreateProductsFormData(formData);
 
     await createProductsSchema.parseAsync({
-      name,
-      description,
-      categoryId,
-      oldPrice,
-      currentPrice,
-      stockQuantity,
-      image01,
-      image02,
+      ...formFields,
     });
 
-    const fieldnameImage01 = image01.name;
+    const fieldnameImage01 = formFields.image01.name;
     const ext1 = path.extname(fieldnameImage01);
     const newFileNameImage01 = `${fieldnameImage01}-${randomUUID()}${ext1}`;
 
-    const fieldnameImage02 = image02.name;
+    const fieldnameImage02 = formFields.image02.name;
     const ext2 = path.extname(fieldnameImage02);
     const newFileNameImage02 = `${fieldnameImage02}-${randomUUID()}${ext2}`;
     // const buffer = await image01.arrayBuffer();
 
-    const fakeUrlImage01 = `http://fake-url/${newFileNameImage01}`;
-    const fakeUrlImage02 = `http://fake-url/${newFileNameImage02}`;
+    const imageUrls = [
+      `http://fake-url/${newFileNameImage01}`,
+      `http://fake-url/${newFileNameImage02}`,
+    ];
 
     const createProductsUseCase = makeCreateProductsUseCase();
     await createProductsUseCase.execute({
-      name,
-      description,
-      categoryId,
-      oldPrice,
-      currentPrice,
-      stockQuantity,
-      imageUrl01: fakeUrlImage01,
-      imageUrl02: fakeUrlImage02,
+      name: formFields.name,
+      description: formFields.description,
+      categoryId: formFields.categoryId,
+      oldPrice: formFields.oldPrice,
+      currentPrice: formFields.currentPrice,
+      stockQuantity: formFields.stockQuantity,
+      imageUrls,
     });
 
     return reply.status(201).send({ message: 'Product created' });
