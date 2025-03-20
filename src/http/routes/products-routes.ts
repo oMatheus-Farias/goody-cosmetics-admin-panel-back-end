d81255c8311dbad41f43b90f16bb90b6964921b1
Matheus <matheusfariasdasneves@gmail.com>
2025-03-20 17:49:27 -0300

    feat: add find all products endpoint

--- src/http/routes/products-routes.ts
+++ src/http/routes/products-routes.ts
@@ -1,8 +1,12 @@
 import type { FastifyInstance } from 'fastify';
 
-import { createProductsController } from '../controllers/products';
+import {
+  createProductsController,
+  findAllProductsController,
+} from '../controllers/products';
 import { verifyJwt } from '../middlewares';
 
 export async function productsRoutes(app: FastifyInstance) {
+  app.get('/', findAllProductsController);
   app.post('/', { onRequest: [verifyJwt] }, createProductsController);
 }
