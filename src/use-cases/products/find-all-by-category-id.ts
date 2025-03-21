9f730dea3a4bd59db9f5b492f89bd76670bab0b0
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-21 14:26:26 -0300

    feat: add FindAllProductsByCategoryIdUseCase

---
+++ src/use-cases/products/find-all-by-category-id.ts
@@ -0,0 +1,27 @@
+import type {
+  CategoriesRepository,
+  ProductsRepository,
+  TProduct,
+} from '../../database/repositories/interfaces';
+import { NotFoundError } from '../../errors';
+import type { TOrdenation } from './interfaces/ordenation-types';
+
+export class FindAllProductsByCategoryIdUseCase {
+  constructor(
+    private readonly productsRepo: ProductsRepository,
+    private readonly categoriesRepo: CategoriesRepository,
+  ) {}
+
+  async execute(
+    categoryId: string,
+    ordernation?: TOrdenation,
+  ): Promise<TProduct[] | null> {
+    const category = await this.categoriesRepo.findById(categoryId);
+
+    if (!category) {
+      throw new NotFoundError('Category not found');
+    }
+
+    return await this.productsRepo.findAllByCategory(categoryId, ordernation);
+  }
+}
