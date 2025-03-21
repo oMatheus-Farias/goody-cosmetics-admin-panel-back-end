a45145e18cd88858b22334d45defa506e907e9b9
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-21 14:27:58 -0300

    feat: add find all products by category id endpoint

--- src/http/routes/products-routes.ts
+++ src/http/routes/products-routes.ts
@@ -2,6 +2,7 @@ import type { FastifyInstance } from 'fastify';
 
 import {
   createProductsController,
+  findAllProductsByCategoryIdController,
   findAllProductsController,
   findAllProductsWithParamsController,
 } from '../controllers/products';
@@ -9,6 +10,11 @@ import { verifyJwt } from '../middlewares';
 
 export async function productsRoutes(app: FastifyInstance) {
   app.get('/', findAllProductsController);
+  app.get(
+    '/category/:categoryId',
+    { onRequest: [verifyJwt] },
+    findAllProductsByCategoryIdController,
+  );
   app.get(
     '/params',
     { onRequest: [verifyJwt] },
