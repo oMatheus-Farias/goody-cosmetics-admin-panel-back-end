43864fbdfe3c6ee619493e4608c515f7238d92ab
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-20 23:45:20 -0300

    feat: add find all products with params endpoint

--- src/http/routes/products-routes.ts
+++ src/http/routes/products-routes.ts
@@ -3,10 +3,16 @@ import type { FastifyInstance } from 'fastify';
 import {
   createProductsController,
   findAllProductsController,
+  findAllProductsWithParamsController,
 } from '../controllers/products';
 import { verifyJwt } from '../middlewares';
 
 export async function productsRoutes(app: FastifyInstance) {
   app.get('/', findAllProductsController);
+  app.get(
+    '/params',
+    { onRequest: [verifyJwt] },
+    findAllProductsWithParamsController,
+  );
   app.post('/', { onRequest: [verifyJwt] }, createProductsController);
 }
