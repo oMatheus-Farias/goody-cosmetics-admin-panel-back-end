38e2a477ea9596fc53159eecdf00778c7f098957
Matheus <matheusfariasdasneves@gmail.com>
2025-03-22 10:30:38 -0300

    test: ensure update product images

---
+++ src/use-cases/products/update-images.spec.ts
@@ -0,0 +1,62 @@
+import type { Category } from '@prisma/client';
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import {
+  InMemoryCategoriesRepository,
+  InMemoryProductsRepository,
+} from '../../database/repositories/in-memory';
+import {
+  CategoriesRepository,
+  ProductsRepository,
+} from '../../database/repositories/interfaces';
+import { CreateCategoriesUseCase } from '../categories/create';
+import type { IProductsDto } from './dtos/products-dto';
+import { UpdateProductsImagesUseCase } from './update-images';
+
+let productsRepo: ProductsRepository;
+let categoriesRepo: CategoriesRepository;
+let categoriesUseCase: CreateCategoriesUseCase;
+let sut: UpdateProductsImagesUseCase;
+let category: Pick<Category, 'id' | 'name'> | null;
+let productData: IProductsDto;
+const categoryName = 'Category Name';
+
+describe('Update Products Images', () => {
+  beforeEach(async () => {
+    productsRepo = new InMemoryProductsRepository();
+    categoriesRepo = new InMemoryCategoriesRepository();
+    categoriesUseCase = new CreateCategoriesUseCase(categoriesRepo);
+    sut = new UpdateProductsImagesUseCase(productsRepo);
+
+    await categoriesUseCase.execute({ name: categoryName });
+    category = await categoriesRepo.findByName(categoryName);
+
+    productData = {
+      name: 'Product Name',
+      description: 'Product Description',
+      categoryId: category!.id,
+      oldPrice: 10,
+      currentPrice: 5,
+      stockQuantity: 10,
+      imageUrls: ['http://image-url.com', 'http://image-url.com'],
+    };
+
+    await productsRepo.create({
+      ...productData,
+      categories: {
+        connect: { id: productData.categoryId },
+      },
+    });
+  });
+
+  it('should update product images', async () => {
+    const response = await productsRepo.findByName(productData.name);
+    const product = await productsRepo.findById(response!.id);
+
+    const newImageUrl = 'http://new-image-url.com';
+
+    await expect(
+      sut.execute(product?.productImage[0].id as string, newImageUrl),
+    ).resolves.toBeUndefined();
+  });
+});
