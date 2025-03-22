6ae622f96bc9b0053c7fe2011cf52e395fda7609
Matheus <matheusfariasdasneves@gmail.com>
2025-03-22 13:24:22 -0300

    feat: add PrismaSalesRepository

---
+++ src/database/repositories/prisma/prisma-sales-repository.ts
@@ -0,0 +1,166 @@
+import { prisma } from '../../../app';
+import type {
+  ICreateSalesDto,
+  IUpdateSalesDto,
+} from '../../../use-cases/sales/dtos';
+import type { SalesRepository, SalesTFindAllWithParams } from '../interfaces';
+
+export class PrismaSalesRepository implements SalesRepository {
+  async findAllWithParams(
+    page: number,
+    searchTerm?: string,
+  ): Promise<SalesTFindAllWithParams> {
+    const sales = (
+      await prisma.sale.findMany({
+        skip: page * 10,
+        take: 10,
+        select: {
+          id: true,
+          saleDate: true,
+          saleItem: {
+            select: {
+              id: true,
+              quantity: true,
+              unitPrice: true,
+              product: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+        where: {
+          saleItem: {
+            some: {
+              product: {
+                name: {
+                  contains: searchTerm,
+                  mode: 'insensitive',
+                },
+              },
+            },
+          },
+        },
+      })
+    ).map((sale) => ({
+      id: sale.id,
+      saleDate: sale.saleDate,
+      totalPrice: sale.saleItem.reduce(
+        (sum, item) => sum + item.quantity * item.unitPrice,
+        0,
+      ),
+      items: sale.saleItem.map((item) => ({
+        saleItemId: item.id,
+        quantity: item.quantity,
+        unitPrice: item.unitPrice,
+        productName: item.product.name,
+      })),
+    }));
+
+    const count = await prisma.sale.count({
+      skip: page * 10,
+      take: 10,
+      where: {
+        saleItem: {
+          some: {
+            product: {
+              name: {
+                contains: searchTerm,
+                mode: 'insensitive',
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const totalCount = await prisma.sale.count({
+      where: {
+        saleItem: {
+          some: {
+            product: {
+              name: {
+                contains: searchTerm,
+                mode: 'insensitive',
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return {
+      sales,
+      meta: {
+        pageIndex: page,
+        limit: 10,
+        countPerPage: count,
+        totalCount,
+      },
+    };
+  }
+  async create(data: ICreateSalesDto): Promise<void> {
+    await prisma.$transaction(async (tx) => {
+      const sale = await tx.sale.create({
+        data: {
+          saleDate: data.saleDate,
+        },
+      });
+
+      const items = data.items.map((item) => {
+        return tx.saleItem.create({
+          data: {
+            quantity: item.quantity,
+            unitPrice: item.unitPrice,
+            productId: item.productId,
+            saleId: sale.id,
+          },
+        });
+      });
+
+      await Promise.all(items);
+    });
+  }
+  async update(data: IUpdateSalesDto): Promise<void> {
+    await prisma.$transaction(async (tx) => {
+      await tx.sale.update({
+        where: {
+          id: data.saleId,
+        },
+        data: {
+          saleDate: data.saleDate,
+        },
+      });
+
+      if (data.items) {
+        const items = data.items.map((item) => {
+          return tx.saleItem.update({
+            where: {
+              id: item.saleItemId,
+            },
+            data: {
+              quantity: item.quantity,
+              unitPrice: item.unitPrice,
+            },
+          });
+        });
+        await Promise.all(items);
+      }
+    });
+  }
+  async deleteSaleItems(saleItemId: string): Promise<void> {
+    await prisma.saleItem.delete({
+      where: {
+        id: saleItemId,
+      },
+    });
+  }
+  async deleteSales(saleId: string): Promise<void> {
+    await prisma.sale.delete({
+      where: {
+        id: saleId,
+      },
+    });
+  }
+}
