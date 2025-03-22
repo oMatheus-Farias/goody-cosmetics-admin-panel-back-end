cdc0cc91c5d2a6f74a9dbba8c2466cb8df2fc77b
Matheus <matheusfariasdasneves@gmail.com>
2025-03-21 22:43:47 -0300

    test: ensure update a product

---
+++ src/use-cases/products/update.spec.ts
@@ -0,0 +1,70 @@
+import type { Category } from '@prisma/client';
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import {
+  InMemoryCategoriesRepository,
+  InMemoryProductsRepository,
+} from '../../database/repositories/in-memory';
+import {
+  CategoriesRepository,
+  ProductsRepository,
+} from '../../database/repositories/interfaces';
+import { CreateCategoriesUseCase } from '../categories/create';
+import type { IProductsDto } from './dtos/products-dto';
+import { UpdateProductsUseCase } from './update';
+
+let productsRepo: ProductsRepository;
+let categoriesRepo: CategoriesRepository;
+let categoriesUseCase: CreateCategoriesUseCase;
+let sut: UpdateProductsUseCase;
+let category: Pick<Category, 'id' | 'name'> | null;
+let productData: IProductsDto;
+const categoryName = 'Category Name';
+
+describe('Update Products', () => {
+  beforeEach(async () => {
+    productsRepo = new InMemoryProductsRepository();
+    categoriesRepo = new InMemoryCategoriesRepository();
+    categoriesUseCase = new CreateCategoriesUseCase(categoriesRepo);
+    sut = new UpdateProductsUseCase(productsRepo, categoriesRepo);
+
+    await categoriesUseCase.execute({ name: categoryName });
+    category = await categoriesRepo.findByName(categoryName);
+
+    productData = {
+      name: 'Product Name',
+      description: 'Product Description',
+      categoryId: category!.id,
+      oldPrice: 10,
+      currentPrice: 5,
+      stockQuantity: 10,
+      imageUrls: ['http://image-url.com', 'http://image-url.com'],
+    };
+
+    await productsRepo.create({
+      ...productData,
+      categories: {
+        connect: { id: productData.categoryId },
+      },
+    });
+  });
+
+  it('should update a product', async () => {
+    const product = await productsRepo.findByName(productData.name);
+
+    await sut.execute(product!.id, {
+      name: 'Updated Product Name',
+      description: 'Updated Product Description',
+      categoryId: category!.id,
+      oldPrice: 20,
+      currentPrice: 15,
+      stockQuantity: 20,
+    });
+
+    const updatedProduct = await productsRepo.findByName(
+      'Updated Product Name',
+    );
+
+    expect(updatedProduct).not.toBeNull();
+  });
+});
