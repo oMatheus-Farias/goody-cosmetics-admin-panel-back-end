85e9d1865cb5b7a7b8b73667bcfa3a869d9c7af9
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-23 21:00:10 -0300

    feat: add update sales endpoint

--- src/http/routes/sales-routes.ts
+++ src/http/routes/sales-routes.ts
@@ -1,8 +1,12 @@
 import type { FastifyInstance } from 'fastify';
 
-import { createSalesController } from '../controllers/sales';
+import {
+  createSalesController,
+  updateSalesController,
+} from '../controllers/sales';
 import { verifyJwt } from '../middlewares';
 
 export async function salesRoutes(app: FastifyInstance) {
   app.post('/', { onRequest: [verifyJwt] }, createSalesController);
+  app.patch('/:saleId', { onRequest: [verifyJwt] }, updateSalesController);
 }
