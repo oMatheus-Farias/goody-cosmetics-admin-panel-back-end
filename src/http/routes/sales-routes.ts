f8108c478459a78d2aeb03e938b54fae02cde14c
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-03-25 16:16:46 -0300

    feat: add find all sales with params endpoint

--- src/http/routes/sales-routes.ts
+++ src/http/routes/sales-routes.ts
@@ -2,11 +2,17 @@ import type { FastifyInstance } from 'fastify';
 
 import {
   createSalesController,
+  findAllSalesWithParamsController,
   updateSalesController,
 } from '../controllers/sales';
 import { verifyJwt } from '../middlewares';
 
 export async function salesRoutes(app: FastifyInstance) {
+  app.get(
+    '/params',
+    { onRequest: [verifyJwt] },
+    findAllSalesWithParamsController,
+  );
   app.post('/', { onRequest: [verifyJwt] }, createSalesController);
   app.patch('/:saleId', { onRequest: [verifyJwt] }, updateSalesController);
 }
