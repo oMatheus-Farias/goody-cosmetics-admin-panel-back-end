f06e1d9c81abe11581ee8cfa0f2d1db059cd2127
Matheus <matheusfariasdasneves@gmail.com>
2025-04-01 19:00:59 -0300

    test: ensure be able to send forgot password email

---
+++ src/use-cases/users/forgot-password.spec.ts
@@ -0,0 +1,52 @@
+import type { $Enums } from '@prisma/client';
+import { beforeEach, describe, expect, it, Mock, vi } from 'vitest';
+
+import { PasswordHasherAdapter } from '../../adapters';
+import type { PasswordHasher } from '../../adapters/interfaces';
+import { InMemoryUsersRepository } from '../../database/repositories/in-memory';
+import type { UsersRepository } from '../../database/repositories/interfaces/users-repository';
+import { sendEmail } from '../../libs/nodemailer/config/mail';
+import { CreateUsersUseCase } from './create';
+import { ForgotPasswordUseCase } from './forgot-password';
+
+let usersRepo: UsersRepository;
+let passwordHasher: PasswordHasher;
+let createUsersUseCase: CreateUsersUseCase;
+let sut: ForgotPasswordUseCase;
+const userData = {
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'johndoe@email.com',
+  password: 'password',
+  role: 'ADMIN' as $Enums.UserRole,
+};
+
+vi.mock('../../libs/nodemailer/config/mail', () => ({
+  sendEmail: vi.fn(),
+}));
+
+describe('Forgot Password', () => {
+  beforeEach(async () => {
+    usersRepo = new InMemoryUsersRepository();
+    passwordHasher = new PasswordHasherAdapter();
+    createUsersUseCase = new CreateUsersUseCase(usersRepo, passwordHasher);
+    sut = new ForgotPasswordUseCase(usersRepo);
+
+    await createUsersUseCase.execute(userData);
+  });
+
+  it('should be able to send forgot password email', async () => {
+    (sendEmail as Mock).mockResolvedValue('Email sending success');
+
+    await expect(
+      sut.execute(userData.email, 'resetToken', new Date()),
+    ).resolves.toBeUndefined();
+    expect(sendEmail).toHaveBeenCalledWith(
+      userData.firstName,
+      userData.email,
+      null,
+      'resetToken',
+      'forgot-password',
+    );
+  });
+});
