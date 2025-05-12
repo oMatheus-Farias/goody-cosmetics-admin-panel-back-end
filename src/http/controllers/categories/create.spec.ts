ee128e057e839318fc01af3e2c1dd2cf471169a4
Matheus <matheusfariasdasneves@gmail.com>
2025-05-12 16:57:26 -0300

    test: (e2e) ensure be able to register a category

--- src/http/controllers/categories/create.spec.ts
+++ src/http/controllers/categories/create.spec.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
 import { app } from '../../../app';
+import { createAndAuthenticatedUser } from '../../../functions/tests/create-and-authenticated-user';
 
 describe('Create Category (e2e)', () => {
   beforeAll(async () => {
@@ -9,6 +10,19 @@ describe('Create Category (e2e)', () => {
   });
   afterAll(async () => {
     await app.close();
+  });
+
+  it('should be able to register a category', async () => {
+    const { token } = await createAndAuthenticatedUser({ app });
+
+    const response = await request(app.server)
+      .post('/api/categories')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Category 1',
+      });
+
+    expect(response.statusCode).toEqual(201);
   });
 
   it('should not be able to register a category, because the user is not authorized', async () => {
